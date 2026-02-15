625338dd02f251cdae1660430573219e24bbf132
dostmiv <mertsondas@windowslive.com>
2026-02-15 17:27:28 +0300

    Initial Commit

---
+++ morningpass/morningpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  morningpassApp.swift
+//  morningpass
+//
+//  Created by Mert Sondaş on 15.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct morningpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ morningpassTests/morningpassTests.swift
@@ -0,0 +1,17 @@
+//
+//  morningpassTests.swift
+//  morningpassTests
+//
+//  Created by Mert Sondaş on 15.02.2026.
+//
+
+import Testing
+@testable import morningpass
+
+struct morningpassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
